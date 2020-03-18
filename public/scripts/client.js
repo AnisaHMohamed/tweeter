e6e8dc4b667a194434a055fef574545f22056481
Anisa <anisahamohamed@gmail.com>
2020-03-17 22:52:46 -0400

    code refractor and styling

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 const escape = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -12,27 +7,26 @@ const escape = function(str) {
 function createTweetElement(tweet) {
   let tweetBody = `<header>
     <div class="nameImg">
-    <img src="${escape(tweet.user.avatars)}" alt="" />
-    <div class="name">${escape(tweet.user.name)}</div>
+        <img src="${escape(tweet.user.avatars)}" alt="" />
+        <div class="name">${escape(tweet.user.name)}</div>
     </div>
     <div class="userName">${escape(tweet.user.handle)}</div>
   </header>
   <p>${escape(tweet.content.text)}</p>
   <footer>
     <div class="daysAgo">${escape(formatTime(tweet.created_at))}</div>
-    <div class="references">
-    <a href='#1'> <i class="far fa-flag"></i> </a>
-    <a href='#2'><i class="fas fa-retweet"></i></a>
-    <a href='#3'> <i class="fas fa-heart"></i></a>
-
-     <div class="wrapper">
-     <div class="heart x1"></div>
-     <div class="heart x2"></div>
-     <div class="heart x3"></div>
-     <div class="heart x4"></div>
-     <div class="heart x5"> </div>
-     <div class="altheart x6"></div>
-   </div>
+      <div class="references">
+        <a href='#1'> <i class="far fa-flag"></i> </a>
+        <a href='#2'><i class="fas fa-retweet"></i></a>
+        <a href='#3'> <i class="fas fa-heart"></i></a>
+        <div class="wrapper">
+        <div class="heart x1"></div>
+        <div class="heart x2"></div>
+        <div class="heart x3"></div>
+        <div class="heart x4"></div>
+        <div class="heart x5"> </div>
+        <div class="altheart x6"></div>
+      </div>
     </div>
   </footer>`;
   return $("<article>")
@@ -41,55 +35,41 @@ function createTweetElement(tweet) {
 }
 //renderTweets
 function renderTweets(tweets) {
-  $("#tweets-container").replaceWith("<section id='tweets-container'/>"); //.empty works too
-
+  $("#tweets-container").replaceWith("<section id='tweets-container'/>"); 
   tweets.forEach(tweet => {
     $("#tweets-container").prepend(createTweetElement(tweet));
   });
 }
-
+//create tweet
 $(document).ready(function() {
   const $form = $("#createTweet");
-
   $(".new-tweet").hide();
-
   $("#writeTweet").click(function() {
     $(".new-tweet").slideToggle("slow", function() {
-      //text area to be hid
-      $("#validInput").removeClass('alert')
-
+      $("#validInput").removeClass("alert");
       $("#textInput").focus();
     });
   });
-
+//tweet post validation
   $form.on("submit", function(event) {
     event.preventDefault();
     const inputText = $("#textInput").val();
 
-
     if (!inputText) {
-      $("#validInput").addClass('alert')
-
+      $("#validInput").addClass("alert");
     } else if (inputText.length > 140) {
-      $("#validInput").removeClass('alert')
-
+      $("#validInput").removeClass("alert");
     } else {
-      
- 
-      $.post("/tweets", $form.serialize())
-      .done(function(data) {
-        $("#counter")
-        .text(140)
+    
+    $.post("/tweets", $form.serialize()).done(function(data) {
+        $("#counter").text(140);
         loadtweets();
         $("#textInput")
-        .val("")
-        .empty();
-        $("#validInput").removeClass('alert')
-      
+          .val("")
+          .empty();
+        $("#validInput").removeClass("alert");
       });
-      
     }
-    
   });
 
   function loadtweets() {
@@ -97,7 +77,6 @@ $(document).ready(function() {
       renderTweets(data);
     });
   }
-
   loadtweets();
 });
 
@@ -106,7 +85,6 @@ const formatTime = milliseconds => {
   const day = hour * 24;
   const month = day * 30;
   const year = month * 12;
-
   const hoursAway = Math.floor((Date.now() - milliseconds) / hour);
   const daysAway = Math.floor((Date.now() - milliseconds) / day);
   const monthsAway = Math.floor((Date.now() - milliseconds) / month);
